fe203a5676c931f8864234d95381b750d4e9c850
Jhonatas <jhonatas1020@gmail.com>
2021-08-04 19:11:35 -0300

    lauout admin criado

---
+++ Locadora/Locadora.MVC/Controllers/HomeAdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Locadora.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locadora.MVC.Controllers
+{
+    public class HomeAdminController: Controller
+    {
+        public IActionResult index() {
+
+            return View();
+        }
+
+        public IActionResult cadastroCliente() {
+            return View();
+        }
+
+        public IActionResult cadastroItens() {
+            return View();
+        }
+    }
+}
